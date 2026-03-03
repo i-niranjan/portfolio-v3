35dbbe220b1af11da20805b82d26440277c06454
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-03-03 16:05:36 +0530

    resized the svg icons

--- app/components/BentoGrid/FirstRow.tsx
+++ app/components/BentoGrid/FirstRow.tsx
@@ -1,6 +1,29 @@
 import React from "react";
-import { ReactIcon } from "./StackIcons";
 
+import { Marquee } from "@/components/ui/marquee";
+import * as StackIcons from "./StackIcons";
+
+const coreStack = [
+  StackIcons.ReactIcon,
+  StackIcons.NextIcon,
+  StackIcons.JsIcon,
+  StackIcons.TsIcon,
+  StackIcons.TailwindIcon,
+  StackIcons.MotionIcon,
+  StackIcons.ExpressIcon,
+  StackIcons.PostgreSqlIcon,
+  StackIcons.MongoDBIcon,
+  StackIcons.DockerIcon,
+  StackIcons.NginxIcon,
+  StackIcons.GithubActionsIcon,
+  StackIcons.RedisIcon,
+  StackIcons.LinuxIcon,
+  StackIcons.ReduxIcon,
+  StackIcons.MedusaIcon,
+  StackIcons.GitIcon,
+  StackIcons.StripeIcon,
+  StackIcons.VercelIcon,
+];
 export default function FirstRow() {
   return (
     <div className=" grid grid-cols-6 h-full">
@@ -9,16 +32,25 @@ export default function FirstRow() {
         <span className="text-sm">Built for scale.</span>
       </div>
       <div className="col-span-5 p-5 flex items-center">
-        <IconBox>
-          <ReactIcon />
-        </IconBox>
+        <Marquee pauseOnHover className="[--duration:60s]">
+          {coreStack.map((Icon, index) => (
+            <IconBox key={index}>
+              <Icon />
+            </IconBox>
+          ))}
+        </Marquee>
       </div>
+      <IconBox>
+        <StackIcons.DockerIcon />
+      </IconBox>
     </div>
   );
 }
 
 const IconBox = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="p-2 rounded-md border glass-icon h-12 w-12">{children}</div>
+    <div className=" flex items-center justify-center   rounded-md border glass-icon w-12 h-12 p-2">
+      {children}
+    </div>
   );
 };
