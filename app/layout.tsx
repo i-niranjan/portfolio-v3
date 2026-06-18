64b1b5980607242b0ef0660950f5478c71c6bce0
i-niranjan <niranjanchaudhari2004@gmail.com>
2026-06-18 09:10:20 +0530

    updated og image setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,12 +48,22 @@ export const metadata: Metadata = {
     siteName: siteConfig.name,
     title: siteConfig.title,
     description: siteConfig.description,
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+        type: "image/png",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.title,
     description: siteConfig.description,
     creator: siteConfig.twitter,
+    images: ["/og-image.png"],
   },
   robots: {
     index: true,
